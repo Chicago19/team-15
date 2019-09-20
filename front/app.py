9adc69ef6e8d43147e9d6e997cc8f647584a6845
camilo <cortiz20@amherst.edu>
2019-09-20 18:31:21 -0500

    added basic Flask app

---
+++ front/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from config import Config
+import threading, webbrowser
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route('/home/', methods=['GET','POST'])
+def renderHome():
+    '''
+    Renders a viewable web application on your port:
+    http://127.0.0.1:5000/home
+    '''
+
+    return (render_template('home.html') )
+
+if __name__ == '__main__':
+    url = 'http://127.0.0.1:5000/home'
+    threading.Timer(2, lambda: webbrowser.open(url, new=1)).start()
+    app.run()
