40281dfa2acea6bb5a90191283fddc914d77a7ed
isadash <anchald2@illinois.edu>
2019-09-20 20:43:55 -0500

    demographic form

--- front/forms.py
+++ front/forms.py
@@ -9,3 +9,18 @@ class AccountCreationForm(FlaskForm):
     username = StringField('Email Address', validators=[DataRequired(), Email()])
     password = StringField('Password', validators=[DataRequired(), Length(min = 6)])
     submit = SubmitField('Submit')
+
+class DemographicForm(FlaskForm):
+    first_name = StringField(u'First Name', validators=[validators.input_required()])
+    last_name  = StringField(u'Last Name', validators=[validators.input_required()])
+    middle_name  = StringField(u'Middle Name', validators=[validators.optional()])
+    date_of_birth = StringField(u'Date of Birth', validators=[validators.input_required()])
+    gender = BooleanField() '''gender'''
+    marital_status = SelectField(u'Programming Language', choices=[('Single', 'Single'),
+    ('Married', 'Married'), ('Divorced', 'Divorced'), ('Widow', 'Widow')])
+    spanish_origin = BooleanField();  '''Are you hispanic or latino?'''
+    country_of_origin = StringField(u'Country of origin', validators=[validators.optional()])
+    racial_group = SelectField(u'Are you from one or more of the following racial groups?',
+    choices=[('American Indian or Alaska Native', 'American Indian or Alaska Native'),
+    ('Asian', 'Asian'), ('Black or African American', 'Black or African American'),
+    ('Native Hawaiian or Pacific Islander', 'Native Hawaiian or Pacific Islander')])
