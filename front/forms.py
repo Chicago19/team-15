f720db4ff7373cdf124fd43738b9cf5680c9be41
Ashley Murray <ashley.j.murray@duke.edu>
2019-09-20 22:40:54 -0500

    form editing

--- front/forms.py
+++ front/forms.py
@@ -6,20 +6,20 @@ class AccountCreationForm(FlaskForm):
     '''
     Flask form used to create an account
     '''
-    username = StringField('Email Address', validators=[DataRequired(), Email()])
-    password = StringField('Password', validators=[DataRequired(), Length(min = 6, message = "Password should be at least 6 characters.")])
-    submit = SubmitField('Submit')
+    username = StringField('Email Address:', validators=[DataRequired(), Email()])
+    password = StringField('Password:', validators=[DataRequired(), Length(min = 6, message = "Password should be at least 6 characters.")])
+    submit = SubmitField('Submit:')
 
 class DemographicForm(FlaskForm):
-    first_name = StringField(u'First Name', validators=[input_required()])
-    last_name  = StringField(u'Last Name', validators=[input_required()])
-    middle_name  = StringField(u'Middle Name', validators=[optional()])
-    date_of_birth = StringField(u'Date of Birth', validators=[input_required()])
+    first_name = StringField(u'First Name:', validators=[input_required()])
+    last_name  = StringField(u'Last Name:', validators=[input_required()])
+    middle_name  = StringField(u'Middle Name:', validators=[optional()])
+    date_of_birth = StringField(u'Date of Birth:', validators=[input_required()])
     gender = BooleanField() #gender
-    marital_status = SelectField(u'Programming Language', choices=[('Single', 'Single'),
+    marital_status = SelectField(u'Marital Status:', choices=[('Single', 'Single'),
     ('Married', 'Married'), ('Divorced', 'Divorced'), ('Widow', 'Widow')])
     spanish_origin = BooleanField();  #Are you hispanic or latino?
-    country_of_origin = StringField(u'Country of origin', validators=[optional()])
+    country_of_origin = StringField(u'Country of origin:', validators=[optional()])
     racial_group = SelectField(u'Are you from one or more of the following racial groups?',
     choices=[('American Indian or Alaska Native', 'American Indian or Alaska Native'),
     ('Asian', 'Asian'), ('Black or African American', 'Black or African American'),
