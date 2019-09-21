c138bd0b7c9ce58e1b92ef0df1927b54506a736d
Ashley Murray <ashley.j.murray@duke.edu>
2019-09-20 21:30:39 -0500

    fixed errors on the validators

--- front/forms.py
+++ front/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, SelectField, validators
-from wtforms.validators import DataRequired, Email, Length, input_required
+from wtforms import StringField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Email, Length, input_required, optional
 
 class AccountCreationForm(FlaskForm):
     '''
@@ -11,15 +11,15 @@ class AccountCreationForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class DemographicForm(FlaskForm):
-    first_name = StringField(u'First Name', validators=[validators.input_required()])
-    last_name  = StringField(u'Last Name', validators=[validators.input_required()])
-    middle_name  = StringField(u'Middle Name', validators=[validators.optional()])
-    date_of_birth = StringField(u'Date of Birth', validators=[validators.input_required()])
+    first_name = StringField(u'First Name', validators=[input_required()])
+    last_name  = StringField(u'Last Name', validators=[input_required()])
+    middle_name  = StringField(u'Middle Name', validators=[optional()])
+    date_of_birth = StringField(u'Date of Birth', validators=[input_required()])
     gender = BooleanField() #gender
     marital_status = SelectField(u'Programming Language', choices=[('Single', 'Single'),
     ('Married', 'Married'), ('Divorced', 'Divorced'), ('Widow', 'Widow')])
     spanish_origin = BooleanField();  #Are you hispanic or latino?
-    country_of_origin = StringField(u'Country of origin', validators=[validators.optional()])
+    country_of_origin = StringField(u'Country of origin', validators=[optional()])
     racial_group = SelectField(u'Are you from one or more of the following racial groups?',
     choices=[('American Indian or Alaska Native', 'American Indian or Alaska Native'),
     ('Asian', 'Asian'), ('Black or African American', 'Black or African American'),
