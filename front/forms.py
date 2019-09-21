407c3d74077907c05f685f64cd98d4a5b290d341
isadash <anchald2@illinois.edu>
2019-09-21 08:18:17 -0500

    no changes

--- front/forms.py
+++ front/forms.py
@@ -17,8 +17,6 @@ class DashboardForm(FlaskForm):
     editprofile = SubmitField('Edit Profile')
     attendance = SubmitField('Attendance')
 
-
-
 orientation = ""
 
 class ScheduleOrientation(FlaskForm):
